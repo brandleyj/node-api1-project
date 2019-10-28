d15ad0d45345007c48b74dbfd96a76ff7d380e4d
Justin Brandley <brandleyj@gmail.com>
2019-10-28 15:55:29 -0500

    MVP

--- index.js
+++ index.js
@@ -74,3 +74,30 @@ server.delete("/api/users/:id", (req, res) => {
 			res.status(500)({ error: "The user could not be removed" })
 		);
 });
+
+server.put("/api/users/:id", (req, res) => {
+	const id = req.params.id;
+	const changes = req.body;
+
+	if (!changes.name || !changes.bio) {
+		res
+			.status(400)
+			.json({ errorMessage: "Please provide name and bio for the user." });
+	} else {
+		db.update(id, changes)
+			.then(dbItem => {
+				if (!dbItem) {
+					res.status(404).json({
+						message: "The user with the specified ID does not exist."
+					});
+				} else {
+					res.status(200).json(db);
+				}
+			})
+			.catch(error => {
+				res.status(500).json({
+					message: "error: The user information could not be modified"
+				});
+			});
+	}
+});
